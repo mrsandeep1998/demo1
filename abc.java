016578f1f50a541ef93ea3b857b2066d05d57f9a
mrsandeep1998 <sandeep2000tiwari@gmail.com>
2022-07-08 16:01:03 +0530

    first

---
+++ abc.java
@@ -0,0 +1,20 @@
+public class abc {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		add(10,20);
+		sub(200,10);
+
+	}
+
+	public static void add(int a ,int b) {
+		System.out.println(a+b);
+	}
+	public static void sub(int a ,int b) {
+		System.out.println(a-b);
+	}
+
+
+}
